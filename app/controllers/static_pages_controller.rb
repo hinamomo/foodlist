c969e76c13c8ad9c637a4332ea5ee0c6c53df61b
momokoishimura <applemelody03@gmail.com>
2016-06-02 02:26:12 +0000

    add foodlist

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
     @user_food = current_user.user_foods.build if logged_in?
+    @user_foods = current_user.user_foods.order(created_at: :desc)
   end
 end
