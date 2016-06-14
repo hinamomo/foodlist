8e5d8470b3fd791fbf8306801cd0a3b54bc82d0e
momokoishimura <applemelody03@gmail.com>
2016-06-14 08:11:16 +0000

    add icon image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,9 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+  
+  def icon_for(icon_id)
+    icon_path = "icon#{icon_id}"
+    image_tag(icon_path, class: "icon")
+  end
 end
